a85b672a04e0b0687018555ff56ba0edeb3adbbb
ZakharDay <zakhar.day@gmail.com>
2026-02-03 03:47:33 +0300

    React UI for a New Swatch form added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css'
 import Cookies from 'js-cookie'
 
+// Cookies.remove('jwt')
+
 function initSubscriptionForm() {
   const form = document.getElementById('subscriptionForm')
   const input = document.querySelector('input[type=email]')
@@ -58,9 +60,41 @@ function authorizeUser() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
+
+        if (data.is_success) {
+          const element = document.createElement('div')
+          element.innerText = `Привет, ${data.email}!`
+
+          const signOutButton = document.createElement('div')
+          signOutButton.classList.add('textButton')
+          signOutButton.innerText = 'Выйти'
+
+          document.body.appendChild(element)
+          document.body.appendChild(signOutButton)
+
+          signOutButton.addEventListener('click', () => {
+            fetch('http://localhost:3000/api/v1/sign_out.json', {
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${jwt}`
+              }
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data)
+
+                element.remove()
+                signOutButton.remove()
+                Cookies.remove('jwt')
+                initLoginForm()
+                initSignupForm()
+              })
+          })
+        }
       })
   } else {
     initLoginForm()
+    initSignupForm()
   }
 }
 
@@ -85,13 +119,41 @@ function initLoginForm() {
 
         if (data.jwt) {
           Cookies.set('jwt', data.jwt)
+          window.location.reload()
+        }
+      })
+  })
+}
+
+function initSignupForm() {
+  const form = document.getElementById('signupForm')
+  const url = form.action
+
+  form.classList.remove('hidden')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+
+        if (data.jwt) {
+          Cookies.set('jwt', data.jwt)
+          window.location.reload()
         }
       })
   })
 }
 
 function initPreviewPage() {
-  const container = document.querySelector('.swatches')
+  const container = document.querySelector('.swatchesSection')
   const url = container.dataset.url
 
   fetch(url)
@@ -118,27 +180,7 @@ function createSwatchPreview(swatchData, container) {
   link.href = `${container.dataset.swatchUri}?swatch=${swatchData.id}`
 
   swatchData.fills.forEach((fillData) => {
-    const fill = document.createElement('div')
-    const color = document.createElement('div')
-    const info = document.createElement('div')
-    const hex = document.createElement('div')
-    const name = document.createElement('div')
-
-    fill.classList.add('fillCard')
-    color.classList.add('fillColor')
-    info.classList.add('fillCardInfo')
-    name.classList.add('fillName')
-
-    hex.innerText = `Colors: ${fillData.colors.length}`
-    name.innerText = `Variable: ${fillData.name}`
-
-    setCssBackgroundValue(color, fillData)
-
-    fill.appendChild(color)
-    fill.appendChild(info)
-    info.appendChild(hex)
-    info.appendChild(name)
-    fills.appendChild(fill)
+    createFillCard(fillData, fills)
   })
 
   h2.appendChild(link)
@@ -147,22 +189,47 @@ function createSwatchPreview(swatchData, container) {
   container.appendChild(swatch)
 }
 
+function createFillCard(data, container) {
+  const fill = document.createElement('div')
+  const color = document.createElement('div')
+  const info = document.createElement('div')
+  const hex = document.createElement('div')
+  const name = document.createElement('div')
+
+  fill.classList.add('fillCard')
+  color.classList.add('fillColor')
+  info.classList.add('fillCardInfo')
+  name.classList.add('fillName')
+
+  fill.dataset.id = data.id
+  hex.innerText = `Colors: ${data.fill_colors.length}`
+  name.innerText = `Variable: ${data.name}`
+
+  setCssBackgroundValue(color, data)
+
+  fill.appendChild(color)
+  fill.appendChild(info)
+  info.appendChild(hex)
+  info.appendChild(name)
+  container.appendChild(fill)
+}
+
 function setCssBackgroundValue(element, fill) {
   let cssColor
 
-  if (fill.colors.length > 1) {
+  if (fill.fill_colors.length > 1) {
     const colors = []
 
-    fill.colors.forEach((color) => {
-      colors.push(`#${color.color} ${color.stop}%`)
+    fill.fill_colors.forEach((fill_color) => {
+      colors.push(`#${fill_color.rgb_hash} ${fill_color.stop}%`)
     })
 
     const cssColors = colors.join(', ')
     cssColor = 'linear-gradient(90deg, ' + cssColors + ')'
 
     element.style.backgroundImage = cssColor
-  } else if (fill.colors.length == 1) {
-    cssColor = '#' + fill.colors[0].color
+  } else if (fill.fill_colors.length == 1) {
+    cssColor = '#' + fill.fill_colors[0].rgb_hash
     element.style.backgroundColor = cssColor
   }
 }
@@ -180,17 +247,142 @@ function initSwatchPage() {
     })
 }
 
+//
+//
+//
+//
+//
+function initNewSwatchFrom() {
+  const container = document.querySelector('.newSwatchForm')
+  const getFillsUrl = container.dataset.getFillsUrl
+  const createSwatchUrl = container.dataset.createSwatchUrl
+  const jwt = Cookies.get('jwt')
+
+  const addFillButton = document.createElement('div')
+  addFillButton.innerText = 'Добавить заливку'
+  addFillButton.classList.add('addFillButton')
+  container.appendChild(addFillButton)
+
+  let fillsContainerVisible = false
+
+  addFillButton.addEventListener('click', () => {
+    if (!fillsContainerVisible) {
+      const headingInput = document.createElement('input')
+      headingInput.placeholder = 'Придумайте название'
+      headingInput.classList.add('headingInput')
+
+      const swatchFillsContainer = document.createElement('div')
+      swatchFillsContainer.classList.add('swatchFillsContainer')
+
+      const fillsContainer = document.createElement('div')
+      fillsContainer.classList.add('fillsContainer')
+
+      const preloader = document.createElement('div')
+      preloader.classList.add('preloader')
+      preloader.innerText = 'Заливки загружаются...'
+
+      const submitButton = document.createElement('div')
+      submitButton.classList.add('submitButton')
+      submitButton.classList.add('disabled')
+      submitButton.innerText = 'Создать палитру'
+
+      container.appendChild(headingInput)
+      fillsContainer.appendChild(preloader)
+      container.appendChild(swatchFillsContainer)
+      container.appendChild(fillsContainer)
+      container.appendChild(submitButton)
+
+      fillsContainerVisible = true
+
+      fetch(getFillsUrl, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+
+          data.forEach((fillData) => {
+            preloader.remove()
+            createFillCard(fillData, fillsContainer)
+
+            const fillCards = document.querySelectorAll('.fillCard')
+
+            fillCards.forEach((fillCard) => {
+              fillCard.addEventListener('click', () => {
+                swatchFillsContainer.appendChild(fillCard)
+              })
+            })
+          })
+
+          submitButton.classList.remove('disabled')
+
+          submitButton.addEventListener('click', () => {
+            const fillCards = swatchFillsContainer.querySelectorAll('.fillCard')
+            const fillIds = []
+
+            fillCards.forEach((card) => {
+              fillIds.push(card.dataset.id)
+            })
+
+            const params = {
+              swatch: {
+                name: headingInput.value,
+                fill_ids: fillIds
+              }
+            }
+
+            fetch(createSwatchUrl, {
+              method: 'POST',
+              headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                Authorization: `Bearer ${jwt}`
+              },
+              body: JSON.stringify(params)
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data)
+
+                const redirectUrl = `http://localhost:8080/swatches/show.html?swatch=${data.swatch_id}`
+                window.location.href = redirectUrl
+              })
+          })
+        })
+    }
+  })
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  if (document.body.classList.contains('index')) {
+  authorizeUser()
+
+  if (
+    document.body.classList.contains('home') &&
+    document.body.classList.contains('index')
+  ) {
     initSubscriptionForm()
-    authorizeUser()
   }
 
-  if (document.body.classList.contains('preview')) {
+  if (
+    document.body.classList.contains('swatches') &&
+    document.body.classList.contains('index')
+  ) {
     initPreviewPage()
   }
 
-  if (document.body.classList.contains('swatch')) {
+  if (
+    document.body.classList.contains('swatches') &&
+    document.body.classList.contains('show')
+  ) {
     initSwatchPage()
+  }
+
+  if (
+    document.body.classList.contains('swatches') &&
+    document.body.classList.contains('new')
+  ) {
+    initNewSwatchFrom()
   }
 })
--- webpack.common.js
+++ webpack.common.js
@@ -55,7 +55,6 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin(),
 
-    // Landing page
     new HtmlWebpackPlugin({
       hash: true,
       scriptLoading: 'blocking',
@@ -64,25 +63,40 @@ module.exports = {
       chunks: ['index']
     }),
 
-    // Preview page
     new HtmlWebpackPlugin({
       hash: true,
       scriptLoading: 'blocking',
-      template: './src/preview.html',
-      filename: './preview.html',
+      template: './src/swatches/index.html',
+      filename: './swatches/index.html',
       chunks: ['index']
     }),
 
-    // Swatch page
     new HtmlWebpackPlugin({
       hash: true,
       scriptLoading: 'blocking',
-      template: './src/swatch.html',
-      filename: './swatch.html',
+      template: './src/swatches/show.html',
+      filename: './swatches/show.html',
+      chunks: ['index']
+    }),
+
+    new HtmlWebpackPlugin({
+      hash: true,
+      scriptLoading: 'blocking',
+      template: './src/swatches/new.html',
+      filename: './swatches/new.html',
       chunks: ['index']
     }),
 
     // Partials
+    new HtmlWebpackPartialsPlugin([
+      {
+        path: path.join(__dirname, './src/partials/user_forms.html'),
+        location: 'user_forms',
+        template_filename: '*',
+        priority: 'replace'
+      }
+    ]),
+
     new HtmlWebpackPartialsPlugin([
       {
         path: path.join(__dirname, './src/partials/analytics.html'),
